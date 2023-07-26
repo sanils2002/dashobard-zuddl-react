f6c13ddddc8a24573006f6f92600f99dbd8f90bd
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2023-07-27 00:24:43 +0530

    Update Logo.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -6,7 +6,7 @@ class Logo extends React.Component {
     return (
       <Link to="/">
         <section className="kanban__logo">
-          <span>fogga</span>
+          <span>Sample</span>
         </section>
       </Link>
     );
